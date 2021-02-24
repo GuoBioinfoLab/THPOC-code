dfa58c6e29e3a00bf007e2b60db4b8700c830ac1
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-02-24 12:03:29 +0800

    vst transform

---
+++ modeling/01-transform.R
@@ -0,0 +1,64 @@
+
+# Library -----------------------------------------------------------------
+
+library(magrittr)
+library(DESeq2)
+
+
+# src ---------------------------------------------------------------------
+
+
+# Function ----------------------------------------------------------------
+
+fn_filter_samples_by_mapped_reads <- function(.se) {
+  .lgl <- .se$`__mapped_reads` > 5e6
+  .se[, .lgl]
+}
+
+fn_filter_genes <- function(.w, .t) {
+  # .w <- wuhan.fs.se
+  # .t <- tom.fs.se
+  .se1 <- .w
+  .se2 <- .t
+  .se1@colData <- .se1@colData[, c("barcode", "class")]
+  .se2@colData <- .se2@colData[, c("barcode", "class")]
+
+  .se <- cbind(.se1, .se2)
+
+  .keep_genes_c <- fn_filter_by_gene_count(.se = .se)
+
+  .se[.keep_genes_c, ]$class %>%
+    levels() %>%
+    purrr::map(.f = fn_filter_by_hyper, .xse) %>%
+    purrr::reduce(.f = dplyr::left_join, by = "name") %>%
+    dplyr::filter_if(.predicate = is.numeric, .vars_predicate = dplyr::all_vars(. < thres)) %>%
+    dplyr::pull(name) ->
+    .keep_genes_ineq
+
+  .keep_genes_ineq
+}
+
+# Load data ---------------------------------------------------------------
+
+wuhan.se <- readr::read_rds(file = "data/rda/wuhan.se.rds.gz")
+tom.se <- readr::read_rds(file = "data/rda/tom.se.rds.gz")
+
+
+# Filter samples by mapped reads ------------------------------------------
+
+
+wuhan.fs.se <- fn_filter_samples_by_mapped_reads(wuhan.se)
+tom.fs.se <- fn_filter_samples_by_mapped_reads(tom.se)
+
+# Filter genes ------------------------------------------------------------
+
+filter_genes <- fn_filter_genes(.w = wuhan.fs.se, .t = tom.fs.se)
+
+wuhan.fs.fg.se <- wuhan.fs.se[filter_genes, ]
+readr::write_rds(x = wuhan.fs.fg.se, file = "data/rda/wuhan.fs.fg.se.rds.gz", compress = "gz")
+tom.fs.fg.se <- tom.fs.se[filter_genes, ]
+readr::write_rds(x = tom.fs.fg.se, file = "data/rda/tom.fs.fg.se.rds.gz", compress = "gz")
+
+# Transform ---------------------------------------------------------------
+
+
