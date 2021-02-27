8db1b345c8f4579c43d23547d4fb93afa4436588
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-02-27 11:53:58 +0800

    new run all

--- run-all.sh
+++ run-all.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 # @AUTHOR: Chun-Jie Liu
 # @CONTACT: chunjie.sam.liu.at.gmail.com
-# @DATE: 2021-03-05 20:24:23
+# @DATE: 2021-03-06 19:31:50
 # @DESCRIPTION:
 
 # Number of input parameters
-
-# jrocker ex 8686
-
 root_dir=/home/liucj/github/THPOC-code
-cutoff_dir=/home/liucj/tmp/THPOC-cutoff
 data_dir=/workspace/liucj/project/09-THPOC
+
 cd ${root_dir}
 
+rm -rf logs/0*
+rm data
+ln -s ${data_dir} data
+
+Rscript ${root_dir}/analysis/01-counts2se.R 1> logs/01-counts2se.R.log 2> logs/01-counts2se.R.err
 
 function fn_run_modeling {
   cf=$1
@@ -21,22 +23,11 @@ function fn_run_modeling {
 
     logname=`basename ${r}`
     echo "Start running ${r} ${cf}."
-    cmd="Rscript ${r} ${cf} 1> data/logs/${logname}.log 2> data/logs/${logname}.err"
+    cmd="Rscript ${r} ${cf} 1> logs/${logname}.log 2> logs/${logname}.err"
     eval ${cmd}
-    # echo ${cmd}
+    echo ${cmd}
     echo "${r} running done."
   done
 }
 
-
-for cutoff in `seq 0.3 0.01 0.5`;
-do
-  rm ${root_dir}/data
-  mkdir -p ${cutoff_dir}/${cutoff}/{logs,output,rda}
-  ln -sf ${cutoff_dir}/${cutoff} ${root_dir}/data
-  ln -sf ${data_dir}/rda/wuhan.se.rds.gz ${root_dir}/data/rda/wuhan.se.rds.gz
-  ln -sf ${data_dir}/rda/tom.se.rds.gz ${root_dir}/data/rda/tom.se.rds.gz
-
-  fn_run_modeling ${cutoff}
-done
-
+fn_run_modeling 0.30
