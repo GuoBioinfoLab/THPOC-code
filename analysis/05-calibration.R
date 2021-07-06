a857fa35bfaba7580042c2e66b48c68f75d7d701
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-07-06 01:41:43 +0000

    calibration

--- analysis/05-calibration.R
+++ analysis/05-calibration.R
@@ -8,5 +8,68 @@
 # Library -----------------------------------------------------------------
 
 library(magrittr)
+library(DESeq2)
+library(mlr)
+library(doParallel)
 library(ggplot2)
+
+# Load data ---------------------------------------------------------------
+
+wuhan.tom.panel.task <- readr::read_rds(file = "data/rda/wuhan.tom.panel.task.rds.gz")
+wuhan.tom.panel.ca125.task <- readr::read_rds(file = "data/rda/wuhan.tom.panel.ca125.task.rds.gz")
+
+panel.model <- readr::read_rds(file = "data/rda/panel.model.rds.gz")
+ca125.model <- readr::read_rds(file = "data/rda/ca125.model.rds.gz")
+panel_ca125.model <- readr::read_rds(file = "data/rda/panel_ca125.model.rds.gz")
+
+
+# Function ----------------------------------------------------------------
+
+fn_predict <- function(.x, .y) {
+  .model <- .x
+  .task <- .y$task
+  .samples <- .y$samples
+
+  purrr::map(
+    .x = names(.samples),
+    .f = function(.x, .model, .task, .samples = .samples) {
+      .pred <- predict(
+        object = .model,
+        task = .task,
+        subset = .samples[[.x]]
+      )
+      .pred
+    },
+    .model = .model,
+    .task = .task,
+    .samples = .samples
+  ) ->
+    .perf
+
+  names(.perf) <- names(.samples)
+  .perf
+}
+
+
+# Task --------------------------------------------------------------------
+bm.task <- c("panel" = list(wuhan.tom.panel.task), wuhan.tom.panel.ca125.task)
+
+
+# Performance --------------------------------------------------------------
+
+purrr::map2(
+  .x = list(panel.model, panel_ca125.model, ca125.model),
+  .y = bm.task,
+  .f = fn_predict
+) ->
+  bm.predict
+
+names(bm.predict) <- c("panel", "panel_ca125", "ca125")
+
+
+# Calibration -------------------------------------------------------------
+
+cal <- generateCalibrationData(bm.predict$panel$TC)
+cal$proportion
+plotCalibration(cal, smooth = TRUE)
 
