9a179f4a4d54116722e482011c39062a626724a2
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-07-11 19:59:59 +0800

    hc and bam

--- analysis/05-calibrate-classifier.R
+++ analysis/05-calibrate-classifier.R
@@ -8,10 +8,9 @@
 # Library -----------------------------------------------------------------
 
 library(magrittr)
+library(ggplot2)
 library(DESeq2)
 library(mlr)
-library(doParallel)
-library(ggplot2)
 
 # src ---------------------------------------------------------------------
 
@@ -19,47 +18,55 @@ source(file = "src/calibrate.R")
 
 # Load data ---------------------------------------------------------------
 
-wuhan.tom.panel.task <- readr::read_rds(file = "data/rda/wuhan.tom.panel.task.rds.gz")
-wuhan.tom.panel.ca125.task <- readr::read_rds(file = "data/rda/wuhan.tom.panel.ca125.task.rds.gz")
-
-panel.model <- readr::read_rds(file = "data/rda/panel.model.rds.gz")
-ca125.model <- readr::read_rds(file = "data/rda/ca125.model.rds.gz")
-panel_ca125.model <- readr::read_rds(file = "data/rda/panel_ca125.model.rds.gz")
-
+bm.hc.task <- readr::read_rds(file = "data/rda/bm.hc.task.rds.gz")
+bm.bam.task <- readr::read_rds(file = "data/rda/bm.bam.task.rds.gz")
+model.list <- readr::read_rds(file = "data/rda/model.list.rds.gz")
 
 # Function ----------------------------------------------------------------
 
 
-
-# Task --------------------------------------------------------------------
-bm.task <- c("panel" = list(wuhan.tom.panel.task), wuhan.tom.panel.ca125.task)
-
-
 # Performance --------------------------------------------------------------
 
+# hc
 purrr::map2(
-  .x = list(panel.model, panel_ca125.model, ca125.model),
-  .y = bm.task,
+  .x = model.list,
+  .y = bm.hc.task,
   .f = fn_get_mlr_pred
 ) ->
-  bm.pred
+  bm.hc.pred
+names(bm.hc.pred) <- c("panel", "panel_ca125", "ca125")
 
-names(bm.pred) <- c("panel", "panel_ca125", "ca125")
+# bam
+purrr::map2(
+  .x = model.list,
+  .y = bm.bam.task,
+  .f = fn_get_mlr_pred
+) ->
+  bm.bam.pred
+names(bm.bam.pred) <- c("panel", "panel_ca125", "ca125")
 
 
 # Calibration -------------------------------------------------------------
 
+# hc
 purrr::map(
-  .x = bm.pred$panel,
+  .x = bm.hc.pred$panel,
   .f = fn_get_calibrate
 ) ->
-  bm.calibrate
+  bm.hc.calibrate
 
+# bam
+purrr::map(
+  .x = bm.bam.pred$panel,
+  .f = fn_get_calibrate
+) ->
+  bm.bam.calibrate
 
 # Calibration plot --------------------------------------------------------
 
+# hc
 tibble::tibble(
-  name = list(names(bm.calibrate)),
+  name = list(names(bm.hc.calibrate)),
   calib = list(c("before", "after"))
 ) %>%
   tidyr::unnest(cols = name) %>%
@@ -69,19 +76,37 @@ tibble::tibble(
     .y = calib,
     .f = function(.x, .y) {
       .title <- glue::glue("{.x} {.y} calibration")
-      .d <- bm.calibrate[[.x]][[.y]]
+      .d <- bm.hc.calibrate[[.x]][[.y]]
       fn_plot_calibration_curve(.x = .d, .title = .title)
     }
   )) ->
-  bm.calibrate.plot
+  bm.hc.calibrate.plot
 
+# bam
+tibble::tibble(
+  name = list(names(bm.bam.calibrate)),
+  calib = list(c("before", "after"))
+) %>%
+  tidyr::unnest(cols = name) %>%
+  tidyr::unnest(cols = calib) %>%
+  dplyr::mutate(calib_plot = purrr::map2(
+    .x = name,
+    .y = calib,
+    .f = function(.x, .y) {
+      .title <- glue::glue("{.x} {.y} calibration")
+      .d <- bm.hc.calibrate[[.x]][[.y]]
+      fn_plot_calibration_curve(.x = .d, .title = .title)
+    }
+  )) ->
+  bm.bam.calibrate.plot
 
 # Save ggplot -------------------------------------------------------------
 
+# hc
 purrr::pwalk(
-  .l = bm.calibrate.plot,
+  .l = bm.hc.calibrate.plot,
   .f = function(name, calib, calib_plot) {
-    .filename <- glue::glue("{name}-{calib}-calibration-plot.pdf")
+    .filename <- glue::glue("BM.HC.{name}-{calib}-calibration-plot.pdf")
 
     ggsave(
       filename = .filename,
@@ -94,6 +119,22 @@ purrr::pwalk(
   }
 )
 
+# bam
+purrr::pwalk(
+  .l = bm.bam.calibrate.plot,
+  .f = function(name, calib, calib_plot) {
+    .filename <- glue::glue("BM.BAM.{name}-{calib}-calibration-plot.pdf")
+
+    ggsave(
+      filename = .filename,
+      plot = calib_plot,
+      device = "pdf",
+      path = "data/reviseoutput/01-model-calibration",
+      width = 5.2,
+      height = 5
+    )
+  }
+)
 
 # Save image --------------------------------------------------------------
 
