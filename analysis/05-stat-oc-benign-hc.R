bffe17ac00fcda752c74cc985225b48016eefe9b
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-07-12 17:33:58 +0800

    rename

---
+++ analysis/05-stat-oc-benign-hc.R
@@ -0,0 +1,81 @@
+# Metainfo ----------------------------------------------------------------
+
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: Mon Jul 12 01:59:18 2021
+# @DESCRIPTION: 05-stat-oc-benign-hc.R
+
+# Library -----------------------------------------------------------------
+
+library(magrittr)
+library(ggplot2)
+library(DESeq2)
+library(mlr)
+
+# Load data ---------------------------------------------------------------
+
+wuhan.se <- readr::read_rds(file = "data/rda/wuhan.se.rds.gz")
+tom.se <- readr::read_rds(file = "data/rda/tom.se.rds.gz")
+wuhan.tom.fs.fg.norm.rbe.se <- readr::read_rds(file = "data/rda/wuhan.tom.fs.fg.norm.rbe.se.rds.gz")
+
+# Function ----------------------------------------------------------------
+
+
+# Stat --------------------------------------------------------------------
+
+wuhan.se@colData %>%
+  as.data.frame() %>%
+  dplyr::mutate(type = plyr::revalue(x = type, replace = c("benign" = "Benign", "malignant" = "Malignant", "normal" = "Health", "unkown" = "Malignant"))) %>%
+  dplyr::select(barcode, oc = oc, type) %>%
+  dplyr::mutate(oc = plyr::revalue(x = oc, replace = c("OC521" = "TC", "OC44" = "DC", "OC79" = "VC1", "OC172" = "VC2"))) ->
+  wuhan.samples
+
+
+tom.se@colData %>%
+  as.data.frame() %>%
+  dplyr::mutate(type = plyr::revalue(x = patientGroup, replace = c("HC" = "Health", "ovarianBenign" = "Benign", "ovarianBorderline" = "Malignant", "ovarianCancer" = "Malignant"))) %>%
+  dplyr::mutate(oc = "VC3") %>%
+  dplyr::select(barcode, oc, type) ->
+  tom.samples
+
+wuhan.tom.samples <- dplyr::bind_rows(wuhan.samples, tom.samples)
+
+
+wuhan.tom.fs.fg.norm.rbe.se@colData %>%
+  as.data.frame() %>%
+  dplyr::inner_join(wuhan.tom.samples, by = "barcode") ->
+  wuhan.tom.used.samples
+
+wuhan.tom.used.samples %>%
+  dplyr::group_by(oc, type) %>%
+  tidyr::nest() %>%
+  dplyr::mutate(age = purrr::map(.x = data, .f = function(.x) {
+    .age <- .x$age
+    .age %>% quantile() -> .q
+    .all <- glue::glue('{.q[3]} ({.q[2]}-{.q[4]})')
+    .q <- .age[.age<=45] %>% quantile()
+    .less45 <- glue::glue('{.q[3]} ({.q[2]}-{.q[4]})')
+    .q <- .age[.age>45] %>% quantile()
+    .greater45 <- glue::glue('{.q[3]} ({.q[2]}-{.q[4]})')
+    tibble::tibble(
+      name = c('Age', '<=45', '>45'),
+      iqr = c(.all, .less45, .greater45)
+    )
+  })) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(-3) %>%
+  tidyr::unnest(age)  ->
+  wuham.tom.used.samples.age
+
+wuham.tom.used.samples.age %>%
+  dplyr::filter(name == "Age") %>%
+  dplyr::select(-name) %>%
+  tidyr::spread(key = oc, value = iqr) ->
+  wuham.tom.used.samples.age.spread
+writexl::write_xlsx(x = wuham.tom.used.samples.age.spread, path = glue::glue("data/reviseoutput/age.xlsx"))
+
+
+
+# Save image --------------------------------------------------------------
+
+save.image(file = "data/rda/05-stat-oc-benign-hc.rda")
---
+++ analysis/08-calibrate-classifier.R
@@ -0,0 +1,98 @@
+# Metainfo ----------------------------------------------------------------
+
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: Mon Jul  5 08:59:57 2021
+# @DESCRIPTION: 08-calibrate-classifier.R
+
+# Library -----------------------------------------------------------------
+
+library(magrittr)
+library(ggplot2)
+library(DESeq2)
+library(mlr)
+
+# src ---------------------------------------------------------------------
+
+source(file = "src/calibrate.R")
+
+# Load data ---------------------------------------------------------------
+
+model.list <- readr::read_rds(file = "data/rda/model.list.rds.gz")
+
+bm.hc.task <- readr::read_rds(file = "data/rda/bm.hc.task.rds.gz")
+bm.bam.task <- readr::read_rds(file = "data/rda/bm.bam.task.rds.gz")
+bm.hc.bam.task <- list(bm.hc = bm.hc.task, bm.bam = bm.bam.task)
+
+el.hc.bam.task <- readr::read_rds(file = "data/rda/el.hc.bam.task.rds.gz")
+epi.hc.bam.task <- readr::read_rds(file = "data/rda/epi.hc.bam.task.rds.gz")
+endo.hc.bam.task <- readr::read_rds(file = "data/rda/endo.hc.bam.task.rds.gz")
+bl.hc.bam.task <- readr::read_rds(file = "data/rda/bl.hc.bam.task.rds.gz")
+glh.hc.bam.task <- readr::read_rds(file = "data/rda/glh.hc.bam.task.rds.gz")
+sr.hc.bam.task <- readr::read_rds(file = "data/rda/sr.hc.bam.task.rds.gz")
+
+# Function ----------------------------------------------------------------
+
+fn_calibrate_pipe <- function(.x, .prefix) {
+  .prefix <- toupper(.prefix)
+  # predict
+  .pred <- purrr::map2(.x = model.list, .y = .x, .f = fn_get_mlr_pred)
+  # calibration
+  .calibrate <- purrr::map(.x = .pred$panel, .f = fn_get_calibrate )
+  # plot
+  .calibrate.plot <- tibble::tibble(
+    name = list(names(.calibrate)),
+    calib = list(c("before", "after"))
+  ) %>%
+    tidyr::unnest(cols = name) %>%
+    tidyr::unnest(cols = calib) %>%
+    dplyr::mutate(calib_plot = purrr::map2(
+      .x = name,
+      .y = calib,
+      .f = function(.x, .y) {
+        .title <- glue::glue("{.x} {.y} calibration")
+        .d <- .calibrate[[.x]][[.y]]
+        fn_plot_calibration_curve(.x = .d, .title = .title)
+      }
+    ))
+  # save plot
+  purrr::pwalk(
+    .l = .calibrate.plot,
+    .f = function(name, calib, calib_plot) {
+      .filename <- glue::glue("{.prefix}.{name}-{calib}-calibration-plot.pdf")
+
+      ggsave(
+        filename = .filename,
+        plot = calib_plot,
+        device = "pdf",
+        path = "data/reviseoutput/01-model-calibration",
+        width = 5.2,
+        height = 5
+      )
+    }
+  )
+}
+
+# Calibrate ---------------------------------------------------------------
+
+list(
+  bm.hc.bam.task,
+  el.hc.bam.task,
+  epi.hc.bam.task,
+  endo.hc.bam.task,
+  bl.hc.bam.task,
+  glh.hc.bam.task,
+  sr.hc.bam.task
+) %>%
+  purrr::walk(.f = function(.l) {
+    names(.l) %>%
+      purrr::walk(.f = function(.name) {
+        fn_calibrate_pipe(.x = .l[[.name]], .prefix = .name)
+      })
+  })
+
+
+# Save image --------------------------------------------------------------
+
+save.image(file = "data/rda/08-calibrate-classifier.rda")
+
